4d6d2cb3b68ca1ad0f29e9eec886bf64d4ae3b49
ANIKET29-G <67858172+ANIKET29-G@users.noreply.github.com>
2020-11-28 20:33:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$title = "Home";
+$content = '<img src="coffee1.png" class="imgLeft" />
+<h3>Title 1</h3>
+<p>
+    We aim to excel in our continual efforts, towards being one of the most recognized institutions, by:
+Providing a conducive environment comprising high-end infrastructure and state-of-the-art laboratory facilities wherein the students, faculty and staff can collectively enhance their technical potential.
+Encouraging innovation through research activities for the benefit of society.
+Developing competent professionals responsive to change in technology.Vestibulum consectetur cursus dui sit amet pretium.
+</p>
+
+<img src="coffee2.png" class="imgRight"/>
+<h3>Title 2</h3>
+<p>
+    We aim to excel in our continual efforts, towards being one of the most recognized institutions, by:
+Providing a conducive environment comprising high-end infrastructure and state-of-the-art laboratory facilities wherein the students, faculty and staff can collectively enhance their technical potential.
+Encouraging innovation through research activities for the benefit of society.
+Developing competent professionals responsive to change in technology.
+    Vivamus elit augue, ullamcorper eget scelerisque vitae, tincidunt eget lorem. Curabitur
+    id nibh libero, sed pulvinar nisi. Curabitur ultrices, neque in dignissim viverra,
+    justo nisl dignissim magna, a tempor eros turpis in diam. Suspendisse potenti. Sed
+    tincidunt est ac elit bibendum nec varius ante vestibulum.
+</p>
+
+<img src="coffee3.png" class="imgLeft" />
+<h3>Title 3</h3>
+<p>We aim to excel in our continual efforts, towards being one of the most recognized institutions, by:
+Providing a conducive environment comprising high-end infrastructure and state-of-the-art laboratory facilities wherein the students, faculty and staff can collectively enhance their technical potential.
+Encouraging innovation through research activities for the benefit of society.
+Developing competent professionals responsive to change in technology.
+</p>';
+
+include 'Template.php';
+?>
+
